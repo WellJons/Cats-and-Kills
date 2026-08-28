6e8c73d98e462c257ea19348cafb7b4065215ee6
WellJons <kromovnikita@gmail.com>
2026-08-28 10:35:13 +0700

    feat: add combat statistics tracking

---
+++ Assets/CatsAndKills/Scripts/Core/CombatStats.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+namespace CatsAndKills.Core
+{
+    public sealed class CombatStats : MonoBehaviour
+    {
+        public static CombatStats Instance { get; private set; }
+
+        public int ShotsFired { get; private set; }
+        public int Hits { get; private set; }
+        public int Kills { get; private set; }
+        public int GrenadesThrown { get; private set; }
+        public float ElapsedSeconds { get; private set; }
+
+        public float Accuracy =>
+            ShotsFired > 0 ? Mathf.Clamp01((float)Hits / ShotsFired) : 0f;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Update()
+        {
+            if (Time.timeScale > 0f)
+                ElapsedSeconds += Time.deltaTime;
+        }
+
+        public void RecordShot()
+        {
+            ShotsFired++;
+        }
+
+        public void RecordHit()
+        {
+            Hits++;
+        }
+
+        public void RecordKill()
+        {
+            Kills++;
+        }
+
+        public void RecordGrenade()
+        {
+            GrenadesThrown++;
+        }
+    }
+}
